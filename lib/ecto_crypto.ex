e33f0cd2903025b6efc003ed84556cf033b5f12e
Mathew Bramson <mathewbramson@gmail.com>
2019-01-05 22:54:06 -0500

    Initial Commit

---
+++ lib/ecto_crypto.ex
@@ -0,0 +1,18 @@
+defmodule EctoCrypto do
+  @moduledoc """
+  Documentation for EctoCrypto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoCrypto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_crypto_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoCryptoTest do
+  use ExUnit.Case
+  doctest EctoCrypto
+
+  test "greets the world" do
+    assert EctoCrypto.hello() == :world
+  end
+end
