5b6a3ae16444c1ab19dc8ec79461849c3064b1af
Mathew Bramson <mathewbramson@gmail.com>
2019-01-05 23:04:51 -0500

    Add mix configuration

--- mix.exs
+++ mix.exs
@@ -6,23 +6,47 @@ defmodule EctoCrypto.MixProject do
       app: :ecto_crypto,
       version: "0.1.0",
       elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      name: "EctoCrypto",
+      source_url: "https://github.com/mbramson/ecto_crypto",
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    """
+    EctoCrypto provides Ecto Types relating to Cryptographic concerns such as
+    encrypted or hashed data.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Mathew Bramson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mbramson/ecto_crypto"}
     ]
   end
 end
